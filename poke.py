a6e04b95c9b4e3503a9db0e905f1746d2221daba
Rushil Perera <pererarushil1940@gmail.com>
2016-07-08 11:13:36 -0400

    Checks if session expired and asks to relogin

--- poke.py
+++ poke.py
@@ -2,11 +2,19 @@ import getpass
 import pickle
 import re
 import time
+import os
 
 import requests
 from bs4 import BeautifulSoup
 
 import login
+
+poke_url = 'https://m.facebook.com/pokes'
+cookies_filename = 'cookies'
+
+
+def delete_cookies(filename):
+    os.remove(filename)
 
 
 def save_cookies(filename):
@@ -20,7 +28,7 @@ def load_cookies(filename):
 
 
 def poke(cookies):
-    poke_url = 'https://m.facebook.com/pokes'
+    global poke_url
     poke_page = session.get(poke_url, cookies=cookies)
     # print poke_page.text
 
@@ -38,25 +46,15 @@ def poke(cookies):
     while True:
         try:
             poke_name = poke_container.findAll('div', {'class': 'bq'})[i + 1] \
-                .findAll('div', {'class': 'bx'})[0].text
+                .findAll('div', {'class': ['bx', 'bw']})[0].text
             substrings = [m.start() for m in
                           re.finditer('( poked you [0-9]+ times in a row)', poke_name.replace(',', ''))]
 
             poke_link = poke_container.findAll('div', {'class': 'bq'})[i] \
                 .findAll('div')[7] \
-                .findAll('a', {'class': 'ca'})[0]['href']
+                .findAll('a', {'class': ['ca', 'bz']})[0]['href']
         except IndexError:
-            try:
-                poke_name = poke_container.findAll('div', {'class': 'bq'})[i] \
-                    .findAll('div', {'class': 'bw'})[0].text
-                substrings = [m.start() for m in
-                              re.finditer('( poked you [0-9]+ times in a row)', poke_name.replace(',', ''))]
-
-                poke_link = poke_container.findAll('div', {'class': 'bq'})[i] \
-                    .findAll('div')[7] \
-                    .findAll('a', {'class': 'bz'})[0]['href']
-            except IndexError:
-                break
+            break
 
         # print poke_container
         # print poke_link
@@ -82,8 +80,13 @@ if __name__ == '__main__':
         print "Loaded Facebook!"
 
         try:
-            cookies = load_cookies('cookies')
-            break
+            cookies = load_cookies(cookies_filename)
+            check_logged_in = session.get(poke_url)
+            if '/login' not in check_logged_in.url:
+                break
+            else:
+                print 'Session has expired. Please log in again.'
+                delete_cookies(cookies_filename)
         except IOError as e:
             if "No such file or directory" not in e.strerror:
                 raise
@@ -100,7 +103,7 @@ if __name__ == '__main__':
 
         if '/login' not in login_request.url:
             if cookies_save:
-                save_cookies('cookies')
+                save_cookies(cookies_filename)
             break
         print 'Your email or password is incorrect. Please try again..'
     print "Logged into Facebook!"
